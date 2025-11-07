c34ab2a72501987dc093d70c3d73b47255990f90
papieros <robertrugala@gmail.com>
2025-11-07 17:40:22 +0100

    cmake script changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,37 @@
+#include "Game.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
-#include "Game.h"
+#include <SDL2/SDL_ttf.h>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
 
 constexpr int SCREEN_WIDTH = 1000;
 constexpr int SCREEN_HEIGHT = 800;
 constexpr int FPS = 60;
 
-int main(int argc, char** args) {
-	{
-		auto game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, FPS);
-		game.initialize();
-		game.run();
-		game.close();
-	}
-	TTF_Quit();
-	IMG_Quit();
-	Mix_Quit();
-	SDL_Quit();
-	return 0;
+void game_loop(void)
+{
+    {
+        auto game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, FPS);
+        game.initialize();
+        game.run();
+        game.close();
+    }
+    TTF_Quit();
+    IMG_Quit();
+    Mix_Quit();
+    SDL_Quit();
+}
+
+int main(int argc, char** args)
+{
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(game_loop, 0, 1);
+#else
+    game_loop();
+#endif
+    return 0;
 }
